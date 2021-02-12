0d0a64443a8ec5b24d42eb20c552f39ae0c4516e
Pauli Jaakkola <pauli.jaakkola@metosin.fi>
2021-02-12 18:00:41 +0200

    Fool around with Reitit controllers and Clip

--- example/simple/src/simplexample/core.cljs
+++ example/simple/src/simplexample/core.cljs
@@ -2,7 +2,22 @@
   (:require [helix.core :refer [defnc $]]
             [helix.hooks :as hooks]
             [helix.dom :as d]
-            ["react-dom" :as rdom]))
+            ["react-dom" :as rdom]
+            [reitit.core :as reitit]
+            [reitit.frontend.controllers :as controllers]
+
+            [ranka.core :as ranka]))
+
+(def routes
+  [["/foo" {:name ::foos
+            :controllers [{::ranka/controller-config
+                           {:components
+                            {:foo {:start (list println "Hello")
+                                   :stop (list println "Goodbye")}}}}]}
+    ["/:id" {:name ::foo
+             :controllers [{::ranka/controller-config {:components {}}}]}]]
+   ["/bar" {:name ::bar
+            :controllers [{::ranka/controller-config {:components {}}}]}]])
 
 ;; define components using the `defnc` macro
 (defnc greeting
@@ -11,14 +26,19 @@
   ;; use helix.dom to create DOM elements
   (d/div "Hello, " (d/strong name) "!"))
 
-(defnc app []
+(defnc app [{:keys [router]}]
   (let [[state set-state] (hooks/use-state {:name "Ranka User"})]
     (d/div
       (d/h1 "Welcome!")
       ;; create elements out of components
       ($ greeting {:name (:name state)})
       (d/input {:value (:name state)
-                :on-change #(set-state assoc :name (.. % -target -value))}))))
+                :on-change #(set-state assoc :name (.. % -target -value))})
+      (d/p (pr-str (ranka/controllers router))))))
 
 (defn ^:export run []
-  (rdom/render ($ app) (js/document.getElementById "app")))
+  (let [router (ranka/clip-controllers (reitit/router routes))
+        foo-match (reitit/match-by-path router "/foo/5")]
+    (rdom/render ($ app {:router router}) (js/document.getElementById "app"))
+    (controllers/apply-controllers [] foo-match)
+    (controllers/apply-controllers (:controllers foo-match) (reitit/match-by-path router "/bar"))))
--- src/ranka/core.cljs
+++ src/ranka/core.cljs
@@ -1,1 +1,21 @@
-(ns ranka.core)
+(ns ranka.core
+  (:require [reitit.core :as reitit]
+            [juxt.clip.core :as clip]))
+
+(defn controllers [router]
+  (->> (reitit/routes router)
+       (map (fn [[_ data]] (select-keys data [:name :controllers])))))
+
+(defn- clip-controller [ctrl]
+  (let [config (::controller-config ctrl)
+        system (volatile! nil)]
+    (-> ctrl
+        (dissoc ::controller-config)
+        (assoc :start (fn [_] (vreset! system (clip/start config)))
+               :stop (fn [_] (clip/stop config @system))))))
+
+(defn clip-controllers [router]
+  (reitit/router
+    (mapv (fn [[path data]] [path (update data :controllers #(mapv clip-controller %))])
+          (reitit/routes router))
+    (reitit/options router)))
